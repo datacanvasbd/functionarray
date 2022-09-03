96d7834a1ebecdd2a7e142d2463595bd62cc861b
sazam <datacanvassbd@gmail.com>
2022-09-03 23:03:13 +0600

    First Function

---
+++ allfunction.php
@@ -0,0 +1,13 @@
+<?php 
+//Encapsulation Hide something
+/**
+ * Function name will be meaningful
+ */
+function isEven($n){ // ($n) Parameter
+    if($n%2==0){
+        return true;
+            }
+      return false;
+      
+  
+  }
---
+++ func1.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once "allfunction.php";
+
+$x=16;
+
+if(isEven($x)){ // passing argument
+    echo "{$x} is an even number";
+       }
+else {
+    echo "{$x} is an odd number";
+     }
+
